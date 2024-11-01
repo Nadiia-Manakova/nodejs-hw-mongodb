55dc84903418c2185abd1c4879311a4fe75a824e
Nadiia-Manakova <nadiiamanakova33@gmail.com>
2024-11-01 20:45:19 +0100

    initial commit

--- src/scripts/removeAllContacts.js
+++
@@ -1,9 +0,0 @@
-import { writeContacts } from '../utils/writeContacts.js';
-
-export const removeAllContacts = async () => {
-  const emptyContacts = [];
-  await writeContacts(emptyContacts);
-  console.log('All contacts have been removed.');
-};
-
-removeAllContacts();
--- src/utils/writeContacts.js
+++
@@ -1,6 +0,0 @@
-import { promises as fs } from 'fs';
-import { PATH_DB } from '../constants/contacts.js';
-
-export const writeContacts = async (updatedContacts) => {
-  await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 5));
-};
