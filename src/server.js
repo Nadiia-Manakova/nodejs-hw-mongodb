1c2ec7fa4670c5a1d7343e8f3bc6896ed68e95bc
Nadiia-Manakova <nadiiamanakova33@gmail.com>
2024-11-02 16:31:37 +0100

    mentor's remarks fixed

--- src/server.js
+++ src/server.js
@@ -33,6 +33,7 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -49,6 +50,7 @@ export const setupServer = () => {
     }
 
     res.status(200).json({
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
