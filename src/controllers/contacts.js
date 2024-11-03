42c3867dfb535950919185795ade0b04828e1793
Nadiia-Manakova <nadiiamanakova33@gmail.com>
2024-11-03 15:25:48 +0100

    route POST created

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -30,3 +30,13 @@ export const getContactByIdController = async (req, res, next) => {
     data: contact,
   });
 };
+
+export const createContactController = async (req, res) => {
+  const contact = await createContact(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: `Successfully created a contact!`,
+    data: contact,
+  });
+};
