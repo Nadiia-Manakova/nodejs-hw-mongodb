41a8e549d0488da29d7bf2989a86f0ff9b9c1dce
Nadiia-Manakova <nadiiamanakova33@gmail.com>
2024-11-06 21:45:43 +0100

    logout and refresh logic added

--- src/server.js
+++ src/server.js
@@ -4,9 +4,10 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { env } from './utils/env.js';
 import router from './routers/index.js';
-
+import cookieParser from 'cookie-parser';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
 dotenv.config();
 
 const PORT = Number(env('PORT', '3000'));
@@ -15,7 +16,7 @@ export const setupServer = () => {
   const app = express();
   app.use(express.json());
   app.use(cors());
-
+  app.use(cookieParser());
   app.use(
     pino({
       transport: {
