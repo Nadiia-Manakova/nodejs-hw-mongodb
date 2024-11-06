476651fb1a50c45c16f474e646afb8bf73e57987
Nadiia-Manakova <nadiiamanakova33@gmail.com>
2024-11-06 20:43:46 +0100

    login logic added

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
-
+import createHttpError from 'http-errors';
 import { UsersCollection } from '../db/models/user.js';
 
 export const registerUser = async (payload) => {
@@ -12,4 +12,16 @@ export const registerUser = async (payload) => {
     ...payload,
     password: encryptedPassword,
   });
+};
+
+export const loginUser = async (payload) => {
+  const user = await UsersCollection.findOne({ email: payload.email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  const isEqual = await bcrypt.compare(payload.password, user.password);
+
+  if (!isEqual) {
+    throw createHttpError(401, 'Unauthorized');
+  }
 };
