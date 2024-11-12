bce5a1b828b6c1da6d04150830a604527661805f
Nadiia-Manakova <nadiiamanakova33@gmail.com>
2024-11-12 22:49:44 +0100

    swagger documentation added

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import cookieParser from 'cookie-parser';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -31,7 +32,9 @@ export const setupServer = () => {
       message: 'Hello World!',
     });
   });
+
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(router);
   app.use('*', notFoundHandler);
   app.use(errorHandler);
